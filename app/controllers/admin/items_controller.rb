154643e6d07e1e30f1bf8ff026468cfac178a708
Mgenki1104 <chobichobin0006@gmail.com>
2021-11-10 12:38:18 +0000

    (11/10)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,35 +2,42 @@ class Admin::ItemsController < ApplicationController
 
   def index
     @items = Item.all
-
+    @genres = Genre.all
   end
 
   def new
     @item = Item.new
+    @genres = Genre.all
+
   end
 
   def create
     @item = Item.new(item_params)
-    @item.genre_id
+    #@item.genre_id
     @item.save
-    redirect_to items_path
+    redirect_to admin_items_path
   end
 
   def show
+    @item = Item.find(params[:id])
+    @items = Item.all
   end
 
   def edit
-     
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
-
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:genre, :name, :image, :introduction, :price, :is_active)
+    params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,3 +10,5 @@ Rails.application.routes.draw do
   devise_for :admin
   root to: 'admin/homes#top'
 end
+
+Refile.secret_key = 'e3c49c3d1efa58fc322021790c2c345243a189b0d58e118bc640c78f5c58a6a540b9a394c641299e1011f44d55e8922baa53548b62ec4becbec17e232b4bacee'
